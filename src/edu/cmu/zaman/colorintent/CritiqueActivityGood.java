8a3d829525094636da01d03b3676b78dc536c7eb
Zachary Aman <zackyuaman@gmail.com>
2014-11-29 14:18:45 -0500

    initial commit

---
+++ src/edu/cmu/zaman/colorintent/CritiqueActivityGood.java
@@ -0,0 +1,15 @@
+package edu.cmu.zaman.colorintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zackaman on 11/22/14.
+ */
+public class CritiqueActivityGood extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_critique_good);
+    }
+}
